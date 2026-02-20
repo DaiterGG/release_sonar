8e9c30aca86b6d865aa1eb9b89f0596a7fc43d80
daiter gd <daitergod19@gmail.com>
2026-02-21 00:23:12 +0300

    basic lambda com

---
+++ public/script.js
@@ -0,0 +1,24 @@
+ID = "http://localhost:3000/request";
+document.getElementById("postBtn").addEventListener("click", function () {
+  // Example HTTPS POST request using fetch
+  fetch(ID, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+      alert("POST request successful. Check console for response.");
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      alert("POST request failed. Check console for error.");
+    });
+});
--- py_lambda/app/app.py
+++ py_lambda/app/app.py
@@ -1,9 +1,21 @@
 import json
 
+def cors_handler(event, context):
+    return {
+        "statusCode": 200,
+        "headers": {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+        },
+        "body": json.dumps({
+            "print": "hello",
+        }),
+    }
 def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "test",
+            "print": "hello",
         }),
     }
