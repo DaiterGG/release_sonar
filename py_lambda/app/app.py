14b9ebb41a044a068011867c95b4ec9ddc7bdc34
daiter gd <daitergod19@gmail.com>
2026-02-22 15:11:10 +0300

    testing ecs launch

--- py_lambda/app/app.py
+++ py_lambda/app/app.py
@@ -1,21 +1,56 @@
 import json
+import boto3
+import os
 
-def cors_handler(event, context):
+def lambda_handler(event, context):
+    client = boto3.client('ecs')
+
+    # Replace with your cluster name
+    cluster_name = 'fargate-sonar'
+    # Replace with the task definition family and revision (or just family to use the latest)
+    task_definition = 'sonar-fargate:8'
+
+    overrides = {
+        'containerOverrides': [
+            {
+                'name': 'daiterdg/release_sonar_rust:latest',
+                'environment': [
+                    {
+                        'name': 'LAUNCH_PARAM_USER_CODE',
+                        'value': 'Hello from Lambda!'
+                    }
+                ]
+            }
+        ]
+    }
+
+    response = client.run_task(
+        cluster=cluster_name,
+        launchType='FARGATE',
+        taskDefinition=task_definition,
+        overrides=overrides,
+        count=1,
+        platformVersion='LATEST',
+        networkConfiguration={
+            'awsvpcConfiguration': {
+                'subnets': [
+                    'subnet-095e23bfdfda7b24f',
+                ],
+                'securityGroups': [
+                    'sg-055134a9b7ae3ee1f'
+                ],
+                'assignPublicIp': 'ENABLED'
+            }
+        }
+    )
+    
+    print("responce from ecs launch");
+    print(response);
+
     return {
         "statusCode": 200,
         "headers": {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+            "Access-Control-Allow-Origin": "https://daitergg.github.io"
         },
-        "body": json.dumps({
-            "print": "hello",
-        }),
-    }
-def lambda_handler(event, context):
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "print": "hello",
-        }),
+        'body': 'Task triggered: ' + response
     }
